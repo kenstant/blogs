389036bb65cee8277c9e49ba43e389163b9b383e
kenstant <xzhm00@163.com>
2024-12-01 20:49:40 +0800

    初始化仓库，第一次提交文件

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    base: '/blogs/',
+    bundler: viteBundler(),
+    theme: defaultTheme(),
+})
